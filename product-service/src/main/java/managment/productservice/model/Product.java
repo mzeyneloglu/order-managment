9a9170c92606b2c1e32ad9dda9932e096661f45e
muhammet.zeyneloglu <muhammet.zeyneloglu@softtech.com.tr>
2023-11-20 16:36:36 +0300

    added product model

---
+++ product-service/src/main/java/managment/productservice/model/Product.java
@@ -0,0 +1,28 @@
+package managment.productservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "MN_PRODUCT")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "DESCRIPTION")
+    private String description;
+    @Column(name = "CATEGORY")
+    private String category;
+    @Column(name = "PRICE")
+    private double price;
+    @Column(name = "DISCOUNT")
+    private BigDecimal discount;
+}
