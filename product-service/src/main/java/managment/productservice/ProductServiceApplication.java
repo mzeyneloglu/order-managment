069f361146262c5cb7b49cf9a596004271bea60f
muhammet.zeyneloglu <muhammet.zeyneloglu@softtech.com.tr>
2023-11-29 09:49:19 +0300

    added apigateway and discovery service & updated product service

--- product-service/src/main/java/managment/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/managment/productservice/ProductServiceApplication.java
@@ -5,9 +5,11 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 	@Bean
 	public OpenAPI openAPI(@Value("${application-version}") String springdocVersion,
--- product-service/src/main/java/managment/productservice/exception/BusinessLogicException.java
+++ product-service/src/main/java/managment/productservice/exception/BusinessLogicException.java
@@ -4,7 +4,4 @@ public class BusinessLogicException extends RuntimeException{
     public BusinessLogicException(String message){
         super(message);
     }
-    public BusinessLogicException (String message, Throwable cause){
-        super(message, cause);
-    }
 }
