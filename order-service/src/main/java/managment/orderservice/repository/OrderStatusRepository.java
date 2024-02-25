24b4666a71d760c9e0cb0bbf9a570dcb6b87c073
muhammet.zeyneloglu <mc.zeyneloglu@gmail.com>
2024-02-25 17:35:21 +0300

    created order repository

---
+++ order-service/src/main/java/managment/orderservice/repository/OrderStatusRepository.java
@@ -0,0 +1,9 @@
+package managment.orderservice.repository;
+
+import managment.orderservice.model.OrderStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderStatusRepository extends JpaRepository<OrderStatus, Long> {
+}
